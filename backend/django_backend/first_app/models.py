d9e8d5834bd193ed3b85f7d91230e15a15e26ca6
Max1216 <max.ruotsalainen@abo.fi>
2025-12-16 23:37:19 +0200

    Fixade problemet med Student och Item tillagd i models.py

--- backend/django_backend/first_app/models.py
+++ backend/django_backend/first_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Student(models.Model):
@@ -28,3 +29,30 @@ class Student(models.Model):
         self.full_clean()
         
         super().save(*args, **kwargs)
+
+class Item(models.Model):
+    class Status(models.TextChoices):
+        ON_SALE = 'ON_SALE', 'on sale'
+        SOLD = 'SOLD', 'Sold'
+    
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    seller = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='items_selling'
+    )
+
+    status = models.CharField(
+        max_length=10,
+        choices=Status.choices,
+        default=Status.ON_SALE,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.status})"
