e4775fd79e471838519c4e088b34fa0342e317d6
Max Ruotsalainen <max.ruotsalainen03@gmail.com>
2025-12-21 15:18:18 +0200

    Added the beginnings of the most popular tab to the homepage

--- React-app/src/components/Home.jsx
+++ React-app/src/components/Home.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
 
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api";
+
+
 function Home() {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch(`${API_BASE_URL}/items/`).then((res) => res.json()).then((data) => {
+            setItems(data.slice(0,4));
+        })
+        .catch(() => {});
+    }, []);
+
+
+
     return (
         <div className="home">
+            {/* HERO */}
             <section className="hero">
                 <div className="hero-content">
                     <h1>
@@ -18,6 +34,31 @@ function Home() {
                         Register To Webshop
                         </Link>
                     </div>
+                </div>
+            </section>
+
+            {/* MOST POPULAR */}
+            <section className="popular">
+                <div className="popular-container">
+                    <p className="popular-subtitle"> EXPLORE AWESOME PRODUCTS</p>
+                    <h2 className="popular-title">MOST POPULAR</h2>
+
+                    <div className="popular-grid">
+                        {items.map((item) => (
+                            <div className="popular-card" key={item.id}>
+                                <div className="popular-image">
+                                    {/* Placeholder image */}
+                                </div>
+
+                                <p className="popular-name">{}item.name</p>
+                                <span className="popular-price">€{item.price}</span>
+                            </div>
+                        ))}
+                    </div>
+
+                    <Link to="/shop" className="popular-button">
+                    Explore Other Products
+                    </Link>
                 </div>
             </section>
         </div>
