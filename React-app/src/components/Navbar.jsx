7e52b6177209b1843adc37cfda8da774811a8bd0
Max Ruotsalainen <max.ruotsalainen03@gmail.com>
2025-12-21 11:34:49 +0200

    Added more functionality. Starting on the style for the homepage next

--- React-app/src/components/Navbar.jsx
+++ React-app/src/components/Navbar.jsx
@@ -1,12 +1,20 @@
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 
 function Navbar() {
     return (
-        <nav>
-            <Link to = "/">Shop</Link>
-            {" | "}
-            <Link to = "/users">Users</Link>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <Link to="/" className='navbar-logo'>
+                WebShop
+                </Link>
+
+                <div className='navbar-links'>
+                    <Link to="/">Shop</Link>
+                    <Link to="/users">Users</Link>
+                </div>
+            </div>
         </nav>
     );
 }
